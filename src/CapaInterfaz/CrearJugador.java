e39aba52c7013e2145d18c800820a5f83236380b
lbrizuela <luisina.brizuela@yahoo.com>
2015-10-02 19:26:27 -0300

    comentario

--- src/CapaInterfaz/CrearJugador.java
+++ src/CapaInterfaz/CrearJugador.java
@@ -37,7 +37,8 @@ import java.awt.event.ActionEvent;
 
 public class CrearJugador extends JFrame {
 
-
+///Luisina
+//Alfo
 	private JPanel contentPane;
 	private JTextField txtRdni;
 	private JTextField txtRnombre;
